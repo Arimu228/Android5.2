da518740674ad00ecf3cd45e6a1997df8049de59
Arik1021 <aaa100107aaa@gmail.com>
2023-01-28 00:29:21 +0600

    second and third commit

--- app/src/main/java/com/example/android52/data/RickAndMortyApi.kt
+++ app/src/main/java/com/example/android52/data/RickAndMortyApi.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Query
 
 interface RickAndMortyApi {
 
-    @GET(" /api/character/{id}")
+    @GET(" /api/character/")
     fun getCharacters(
         @Query("page")page:Int
     ): Call<Characters>
